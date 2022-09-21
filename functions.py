82c7a06089b4ea932db86db75e48b5683b40605c
VadimNasibullin <vadim-on@mail.ru>
2022-09-21 10:40:53 +0500

    'Editing'

--- functions.py
+++ functions.py
@@ -1,10 +1,10 @@
 def get_summ(one, two, delimiter ='&'):
-       print(one.upper(), delimiter, two.upper()) 
+    print(one.upper(), delimiter, two.upper()) 
 
 get_summ('learn', 'python')
 
 
 def format_price(price):
-       print('Цена:', int(price), 'руб.')
+    print('Цена:', int(price), 'руб.')
 
 format_price(56.24)       
